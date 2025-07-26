5b9ca080aaf1e151f34bcb69e683f66010d1e2e4
MoisesBM20 <moisesbuitrago201@gmail.com>
2025-07-25 19:48:32 -0500

    Actualizacion de hero 1

---
+++ src/app/components/pqrs/pqrs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pqrs',
+  imports: [],
+  templateUrl: './pqrs.html',
+  styleUrl: './pqrs.scss'
+})
+export class Pqrs {
+
+}
