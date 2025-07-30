62a11e79cb965517ab4574eb359d056f73480538
Buitrago Mosquera <mbuitrago@REDCOLSA.LOCAL>
2025-07-30 16:48:32 -0500

    Actualizacion general de diseño

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 import { Navbar } from './layout/navbar/navbar';
 import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, MatIconModule, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
