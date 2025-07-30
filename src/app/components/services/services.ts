c7a7d12278eeca2eeb6b8cd1b4a731fa29073635
Buitrago Mosquera <mbuitrago@REDCOLSA.LOCAL>
2025-07-30 08:40:37 -0500

    m

--- src/app/components/services/services.ts
+++ src/app/components/services/services.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  standalone: true, // <-- Esto es importante si usas standalone components
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './services.html',
   styleUrl: './services.scss'
 })
-export class Services {
-
-}
+export class Services {}
