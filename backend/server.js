07a4b2f284594739591689bae5297385118483a9
Buitrago Mosquera <mbuitrago@REDCOLSA.LOCAL>
2025-08-05 11:16:47 -0500

    Definicion de puertos

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,15 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+const express = require('express');
 const path = require('path');
+const app = express();
+const port = process.env.PORT || 4000;
+
 app.use(express.static(path.join(__dirname, '../dist/IPSWeb')));
 
-app.get('*', (req, res) => {
+// Usa expresión regular para evitar el error
+app.get(/^\/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/IPSWeb/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
