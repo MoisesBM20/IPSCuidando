6b14b4bc58f11619514a90d27abcb04778252513
Buitrago Mosquera <mbuitrago@REDCOLSA.LOCAL>
2025-08-05 11:06:56 -0500

    Definicion de puertos

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../dist/IPSWeb')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/IPSWeb/index.html'));
+});
