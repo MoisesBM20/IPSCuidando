2a82ab0afe9663318f253ccea3d1b565c0e72cac
Buitrago Mosquera <mbuitrago@REDCOLSA.LOCAL>
2025-08-08 10:36:46 -0500

    Actualizacion de mejoras Generales

--- src/app/components/services/services.ts
+++ src/app/components/services/services.ts
@@ -3,10 +3,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-services',
   standalone: true, // <-- Esto es importante si usas standalone components
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, CommonModule, RouterModule],
   templateUrl: './services.html',
   styleUrl: './services.scss'
 })
